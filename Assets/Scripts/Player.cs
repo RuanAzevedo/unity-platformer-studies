3e3a2f9fbfe2318d98cea4f63f7e3653de1938ec
Ruan Azevedo <ruan.azevedo@gmail.com>
2023-05-26 12:49:13 -0300

    ✨ Added IsGrounded to the player
    And made the jump check for grounding state

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,11 +6,12 @@ public class Player : MonoBehaviour
     [SerializeField] private float _jumpVelocity = 5;
     [SerializeField] private float _jumpDuration = 0.5f;
 
+    public bool IsGrounded;
+
     void OnDrawGizmos()
     {
         SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
-        float bottomY = spriteRenderer.bounds.extents.y;
-        Vector2 origin = new Vector2(transform.position.x, transform.position.y - bottomY);
+        Vector2 origin = new Vector2(transform.position.x, transform.position.y - spriteRenderer.bounds.extents.y);
 
         Gizmos.color = Color.red;
         Gizmos.DrawLine(origin, origin + Vector2.down * 0.1f);
@@ -19,11 +20,20 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
+        Vector2 origin = new Vector2(transform.position.x, transform.position.y - spriteRenderer.bounds.extents.y);
+        var hit = Physics2D.Raycast(origin, Vector2.down, 0.1f);
+
+        if (hit.collider)
+            IsGrounded = true;
+        else
+            IsGrounded = false;
+
         var horizontal = Input.GetAxis("Horizontal");
         Rigidbody2D rb = GetComponent<Rigidbody2D>();
         var vertical = rb.velocity.y;
 
-        if (Input.GetButtonDown("Fire1"))
+        if (Input.GetButtonDown("Fire1") && IsGrounded)
             _jumpEndTime = Time.time + _jumpDuration;
 
         if (Input.GetButton("Fire1") && _jumpEndTime > Time.time)
